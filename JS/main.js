41bdb5d63d47bc5233d76108da781f6409f646e6
Akiva Abramson <akivaabramson@gmail.com>
2023-06-22 13:09:02 +0300

    css fixes

--- JS/main.js
+++ JS/main.js
@@ -7,6 +7,9 @@ function onInit(){
     gElCanvas = document.querySelector('#canvas')
     gCtx = gElCanvas.getContext('2d')
     renderMeme()
+    // window.addEventListener('resize', ()=>{
+    //     resizeCanvas()
+    // })
 }
 
 function logoClicked(){
@@ -17,4 +20,13 @@ function logoClicked(){
 
 function toggleMenu() {
     document.body.classList.toggle('menu-open');
+}
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+    // Note: changing the canvas dimension this way clears the canvas
+    // console.log('elContainer.offsetWidth:', elContainer.offsetWidth)
+    gElCanvas.width = elContainer.offsetWidth
+    // Unless needed, better keep height fixed.
+    // gElCanvas.height = elContainer.offsetHeight
 }
